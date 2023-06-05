53b3d6bba9fe69a06bbd3ba2965d38f9d2f5d704
ManojHT <manulogan101@gm.com>
2023-06-05 15:21:01 +0530

    List Update

--- src/app/user/create/create.component.ts
+++ src/app/user/create/create.component.ts
@@ -15,13 +15,32 @@ export class CreateComponent {
     private formBuilder : FormBuilder
     ){
       this.userDetails = formBuilder.group({
-        name : new FormControl(),
+        firstName : new FormControl(),
+        lastName : new FormControl(),
         password : new FormControl(),
-        mobileNumber : new FormControl()
+        mobileNumber : new FormControl(),
+        emailId : new FormControl()
       })
     }
 
   submit(){
     console.log(this.userDetails.value)
+    let url = 'http://localhost:3000/userDetails'
+    let body = this.userDetails.value
+    this.http.post(url, body).subscribe({
+      next : (arg) => {
+        console.log(arg)
+      }
+    })
+  }
+
+  delete(){
+    let url = 'http://localhost:3000/userDetails'
+    let body = {"id" : "1"}
+    this.http.delete(url).subscribe({
+      next : (res) => {
+          console.log(res)
+      }
+    })
   }
 }
--- src/app/user/list/list.component.ts
+++ src/app/user/list/list.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.scss']
 })
-export class ListComponent {
+export class ListComponent implements OnInit {
+
+  constructor (
+    private http : HttpClient,
+    private router : Router
+  ) {
+
+  }
+  ngOnInit(): void {
+    this.fetchDetails()
+  }
+
+  userDetails : any
+  fetchDetails(){
+    let url = 'http://localhost:3000/userDetails'
+    this.http.get(url).subscribe({
+      next : (res) => {
+          this.userDetails = res
+          for(let ele of this.userDetails){
+            if(ele['firstName'] == 'Shubham'){
+              if(ele['password'] == '123'){
+                this.router.navigate(['reviewer','list'])
+              }
+            }
+          }
+      }
+    })
+  }
 
 }
